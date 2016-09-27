b8d4a23fe53cbc1d2fbc5f17a0223fda3788cd6c
andy <andy@andydote.co.uk>
2016-09-27 20:56:15 +0300

    httpMethodOverride: implement whitelist of allowed method overrides

--- Granger.Tests/Decorators/HttpMethodOverrideTests.cs
+++ Granger.Tests/Decorators/HttpMethodOverrideTests.cs
@@ -13,10 +13,10 @@ namespace Granger.Tests.Decorators
 {
 	public class HttpMethodOverrideTests : IDisposable
 	{
-		private readonly TestServer _server;
+		private TestServer _server;
 		private IOwinRequest _request;
 
-		public HttpMethodOverrideTests()
+		private void CreateServer()
 		{
 			_server = TestServer.Create(app =>
 			{
@@ -29,10 +29,27 @@ namespace Granger.Tests.Decorators
 			});
 		}
 
+		private void CreateServerWithWhitelist(params string[] methods)
+		{
+			_server = TestServer.Create(app =>
+			{
+				app.Use<HttpMethodOverride>(new object[] { methods });
+
+				app.Run(async context =>
+				{
+					_request = context.Request;
+					await Task.FromResult(0);
+				});
+			});
+		}
+
 		[Fact]
 		public async Task When_the_route_has_no_override()
 		{
+			CreateServer();
+
 			var response = await _server.HttpClient.GetAsync("resource");
+
 			response.StatusCode.ShouldBe(HttpStatusCode.OK);
 			_request.Method.ShouldBe("GET");
 		}
@@ -45,6 +62,8 @@ namespace Granger.Tests.Decorators
 		[InlineData("head")]
 		public async Task When_the_route_has_an_override_and_is_not_a_post(string method)
 		{
+			CreateServer();
+
 			var response = await _server.CreateRequest("resource?_method=DELETE").SendAsync(method);
 
 			response.StatusCode.ShouldBe(HttpStatusCode.OK);
@@ -59,6 +78,36 @@ namespace Granger.Tests.Decorators
 		[InlineData("head")]
 		public async Task When_the_route_has_an_override_and_is_a_post(string method)
 		{
+			CreateServer();
+
+			var response = await _server.CreateRequest("resource?_method=" + method).SendAsync("POST");
+
+			response.StatusCode.ShouldBe(HttpStatusCode.OK);
+			_request.Method.ShouldBe(method);
+		}
+
+		[Theory]
+		[InlineData("get")]
+		[InlineData("put")]
+		[InlineData("head")]
+		public async Task When_using_a_restricted_method_list_and_the_supplied_method_is_not_whitelisted(string method)
+		{
+			CreateServerWithWhitelist("DELETE", "PATCH");
+
+			var response = await _server.CreateRequest("resource?_method=" + method).SendAsync("POST");
+
+			response.StatusCode.ShouldBe(HttpStatusCode.MethodNotAllowed);
+
+			_request.ShouldBeNull();
+		}
+
+		[Theory]
+		[InlineData("patch")]
+		[InlineData("delete")]
+		public async Task When_using_a_restricted_method_list_and_the_supplied_method_is_whitelisted(string method)
+		{
+			CreateServerWithWhitelist("DELETE", "PATCH");
+
 			var response = await _server.CreateRequest("resource?_method=" + method).SendAsync("POST");
 
 			response.StatusCode.ShouldBe(HttpStatusCode.OK);
--- Granger/Decorators/HttpMethodOverride.cs
+++ Granger/Decorators/HttpMethodOverride.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Owin;
@@ -7,19 +10,47 @@ namespace Granger.Decorators
 {
 	public class HttpMethodOverride : OwinMiddleware
 	{
-		public HttpMethodOverride(OwinMiddleware next) : base(next)
+		private static readonly string[] DefaultMethods = new[]
 		{
+			"get",
+			"put",
+			"patch",
+			"delete",
+			"head"
+		};
+
+		private HashSet<string> _allowedMethods;
+
+		public HttpMethodOverride(OwinMiddleware next) : this(next, DefaultMethods)
+		{
+		}
+
+		public HttpMethodOverride(OwinMiddleware next, IEnumerable<string> allowedMethods) : base(next)
+		{
+			_allowedMethods = new HashSet<string>(allowedMethods, StringComparer.OrdinalIgnoreCase);
 		}
 
 		public override async Task Invoke(IOwinContext context)
 		{
 			if (context.Request.Method == HttpMethod.Post.Method)
-				context.Request.Method = context
+			{
+				var overrideMethod = context
 					.Request
 					.Query
 					.GetValues("_method")
 					.DefaultIfEmpty(HttpMethod.Post.Method)
 					.First();
+
+				if (_allowedMethods.Contains(overrideMethod) == false)
+				{
+					context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+					context.Response.Headers[HttpResponseHeader.Allow.ToString()] = string.Join(", ", _allowedMethods);
+					return;
+				}
+
+				context.Request.Method = overrideMethod;
+
+			}
 
 			await Next.Invoke(context);
 		}
