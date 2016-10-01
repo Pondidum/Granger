e7b0a65f840ed28adefeba5b07c8aff6bbcd5a4e
andy <andy@andydote.co.uk>
2016-10-01 22:15:26 +0300

    conformity: add url finder class

---
+++ Granger.Tests/Conformity/UrlFinderTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using Granger.Conformity;
+using Newtonsoft.Json.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Granger.Tests.Conformity
+{
+	public class UrlFinderTests
+	{
+		private readonly UrlFinder _finder;
+
+		public UrlFinderTests()
+		{
+			_finder = new UrlFinder();
+		}
+
+		[Fact]
+		public void When_given_a_blank_object()
+		{
+			_finder
+				.Execute(JToken.FromObject(new {}))
+				.ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_given_an_object_with_a_single_string_url()
+		{
+			_finder
+				.Execute(JToken.FromObject(new { location = "http://example.com" }))
+				.Single()
+				.Path
+				.ShouldBe("location");
+		}
+
+		[Fact]
+		public void When_given_an_object_with_a_non_url_string()
+		{
+			_finder
+				.Execute(JToken.FromObject(new { location = "something not a url" }))
+				.ShouldBeEmpty();
+		}
+
+		[Fact]
+		public void When_a_url_is_in_a_sub_object()
+		{
+			_finder
+				.Execute(JToken.FromObject(new { inner = new { location = "http://exmaple.com" } }))
+				.Single()
+				.Path
+				.ShouldBe("inner.location");
+		}
+	}
+}
---
+++ Granger/Conformity/UrlFinder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Granger.Decorators;
+using Newtonsoft.Json.Linq;
+
+namespace Granger.Conformity
+{
+	public class UrlFinder
+	{
+		public IEnumerable<JToken> Execute(JToken token)
+		{
+			var rx = new Regex("^https?://");
+
+			return Find(token, t => t.Type == JTokenType.String)
+				.Where(t => rx.IsMatch(t.ToString()))
+				.Where(t => t.Path.Split('.').Last() != "href");
+		}
+
+		private IEnumerable<JToken> Find(JToken token, Func<JToken, bool> condition)
+		{
+			foreach (var child in token.Children())
+			{
+				if (condition(child))
+					yield return child;
+
+				foreach (var inner in Find(child, condition))
+					yield return inner;
+			}
+		}
+	}
+}
