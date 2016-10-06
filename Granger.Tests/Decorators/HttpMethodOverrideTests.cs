12127b54ccdb4d696cca68f2ad16715e14be2d87
andy <andy@andydote.co.uk>
2016-10-06 18:31:13 +0300

    responseHeaderValidator: implement validator

--- Granger.Tests/Decorators/HttpMethodOverrideTests.cs
+++ Granger.Tests/Decorators/HttpMethodOverrideTests.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Granger.Decorators;
 using Microsoft.Owin;
---
+++ Granger/Decorators/ResponseHeaderValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+
+namespace Granger.Decorators
+{
+	public class ResponseHeaderValidator : OwinMiddleware
+	{
+		public ResponseHeaderValidator(OwinMiddleware next) : base(next)
+		{
+		}
+
+		public override async Task Invoke(IOwinContext context)
+		{
+			await Next.Invoke(context);
+
+			if (string.IsNullOrWhiteSpace(context.Response.ContentType))
+			{
+				var response = context.Response;
+
+				response.StatusCode = (int)HttpStatusCode.InternalServerError;
+				response.ContentType = "application/json";
+
+				var json = JsonConvert.SerializeObject(new
+				{
+					Message = "The response was missing a recommend header: Content-Type"
+				});
+
+				await response.WriteAsync(json);
+			}
+
+			await Task.Yield();
+		}
+	}
+}
