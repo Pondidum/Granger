449514b8e79842301450075c641bc93b651599c5
andy <andy@andydote.co.uk>
2016-10-07 21:39:12 +0300

    decorators: add requestHeaderValidator

---
+++ Granger.Tests/Decorators/RequestHeaderValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Granger.Decorators;
+using Microsoft.Owin;
+using Microsoft.Owin.Testing;
+using Newtonsoft.Json.Linq;
+using Owin;
+using Shouldly;
+using Xunit;
+
+namespace Granger.Tests.Decorators
+{
+	public class RequestHeaderValidatorTests
+	{
+		private readonly TestServer _server;
+
+		public RequestHeaderValidatorTests()
+		{
+			_server = TestServer.Create(app =>
+			{
+				app.Use<RequestHeaderValidator>();
+				app.Run(async context =>
+				{
+					await Task.Yield();
+				});
+			});
+		}
+
+		[Fact]
+		public async Task When_the_request_has_no_accept()
+		{
+			var response = await _server
+				.CreateRequest("resource")
+				.GetAsync();
+			var content = JToken.Parse(await response.Content.ReadAsStringAsync());
+
+			response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+			content["Message"].ShouldBe("The response was missing a recommend header: Accept");
+		}
+
+		[Fact]
+		public async Task When_the_request_has_a_accept()
+		{
+			var response = await _server
+				.CreateRequest("resource")
+				.AddHeader("Accept", "application/json")
+				.GetAsync();
+			var content = await response.Content.ReadAsStringAsync();
+
+			content.ShouldBe("");
+		}
+	}
+}
---
+++ Granger/Decorators/RequestHeaderValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+
+namespace Granger.Decorators
+{
+	public class RequestHeaderValidator : OwinMiddleware
+	{
+		public RequestHeaderValidator(OwinMiddleware next) : base(next)
+		{
+		}
+
+		public override async Task Invoke(IOwinContext context)
+		{
+			var request = context.Request;
+
+			if (string.IsNullOrWhiteSpace(request.Accept))
+			{
+				var response = context.Response;
+				response.StatusCode = (int)HttpStatusCode.BadRequest;
+				response.ContentType = "application/json";
+
+				var json = JsonConvert.SerializeObject(new
+				{
+					Message = "The response was missing a recommend header: Accept"
+				});
+
+				await response.WriteAsync(json);
+				return;
+			}
+
+			await Next.Invoke(context);
+		}
+	}
+}
