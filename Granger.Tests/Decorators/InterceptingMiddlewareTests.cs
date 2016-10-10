b0234de6b983d527f80860b41afc1fd323d7ba80
andy <andy@andydote.co.uk>
2016-10-10 21:28:09 +0300

    tests: add tests for an intercepting middleware

---
+++ Granger.Tests/Decorators/InterceptingMiddlewareTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Granger.Decorators;
+using Microsoft.Owin;
+using Microsoft.Owin.Testing;
+using Owin;
+using Shouldly;
+using Xunit;
+
+namespace Granger.Tests.Decorators
+{
+	public class InterceptingMiddlewareTests : IDisposable
+	{
+		private TestServer _server;
+		private Action<IOwinRequest, IOwinResponse> _innerHandle;
+		private Func<IOwinContext, MemoryStream, Task<MemoryStream>> _postHandle;
+
+		private async Task<HttpResponseMessage> Get()
+		{
+			_server = TestServer.Create(app =>
+			{
+				app.Use<TestInterceptMiddleware>(_postHandle);
+				app.Run(async context =>
+				{
+					_innerHandle(context.Request, context.Response);
+					await Task.Yield();
+				});
+			});
+
+			return await _server.HttpClient.GetAsync("/resource");
+		}
+
+		[Fact]
+		public async Task When_the_post_intercept_does_nothing()
+		{
+			var content = "This is the content";
+			_innerHandle = (req, res) => res.Write(content);
+
+			var response = await Get();
+			var responseContent = await response.Content.ReadAsStringAsync();
+
+			responseContent.ShouldBe(content);
+		}
+
+		[Fact]
+		public async Task When_the_post_intercept_returns_the_stream_at_the_beginning()
+		{
+			var content = "This is the content";
+			_innerHandle = (req, res) => res.Write(content);
+			_postHandle = async (context, stream) =>
+			{
+				await Task.Yield();
+				stream.Position = 0;
+				return stream;
+			};
+
+			var response = await Get();
+			var responseContent = await response.Content.ReadAsStringAsync();
+
+			responseContent.ShouldBe(content);
+		}
+
+		[Fact]
+		public async Task When_the_post_intercept_returns_the_stream_at_the_end()
+		{
+			var content = "This is the content";
+			_innerHandle = (req, res) => res.Write(content);
+			_postHandle = async (context, stream) =>
+			{
+				await Task.Yield();
+				stream.Position = stream.Length;
+				return stream;
+			};
+
+			var response = await Get();
+			var responseContent = await response.Content.ReadAsStringAsync();
+
+			responseContent.ShouldBe(content);
+		}
+
+		[Fact]
+		public async Task When_the_interceptor_replaces_the_stream()
+		{
+			var replacement = "This is the replacement";
+
+			_innerHandle = (req, res) => res.Write("This is the content");
+			_postHandle = async (context, stream) =>
+			{
+				await Task.Yield();
+				return new MemoryStream(Encoding.UTF8.GetBytes(replacement));
+			};
+
+			var response = await Get();
+			var responseContent = await response.Content.ReadAsStringAsync();
+
+			responseContent.ShouldBe(replacement);
+		}
+
+		public void Dispose()
+		{
+			_server.Dispose();
+		}
+
+		private class TestInterceptMiddleware : InterceptingMiddleware
+		{
+			private readonly Func<IOwinContext, MemoryStream, Task<MemoryStream>> _handle;
+
+			public TestInterceptMiddleware(OwinMiddleware next, Func<IOwinContext, MemoryStream, Task<MemoryStream>> handle) : base(next)
+			{
+				_handle = handle;
+			}
+
+			protected override Task<MemoryStream> AfterNext(IOwinContext context, MemoryStream internalMiddleware)
+			{
+				if (_handle == null)
+					return base.AfterNext(context, internalMiddleware);
+				else
+					return _handle(context, internalMiddleware);
+			}
+		}
+	}
+}
