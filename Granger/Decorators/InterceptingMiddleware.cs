0bb06fb5778eac6dfaac5096f78eab7785f64882
andy <andy@andydote.co.uk>
2016-10-10 21:28:46 +0300

    interceptingMiddleware: implement!

---
+++ Granger/Decorators/InterceptingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Granger.Decorators
+{
+	public class InterceptingMiddleware : OwinMiddleware
+	{
+		public InterceptingMiddleware(OwinMiddleware next) : base(next)
+		{
+		}
+
+		public override async Task Invoke(IOwinContext context)
+		{
+			var stream = context.Response.Body;
+			var buffer = new MemoryStream();
+
+			context.Response.Body = buffer;
+
+			await Next.Invoke(context);
+
+			var intercepted = await AfterNext(context, buffer);
+			intercepted.Position = 0;
+
+			await intercepted.CopyToAsync(stream);
+		}
+
+		protected virtual async Task<MemoryStream> AfterNext(IOwinContext context, MemoryStream internalMiddleware)
+		{
+			await Task.Yield();
+			return internalMiddleware;
+		}
+	}
+}
