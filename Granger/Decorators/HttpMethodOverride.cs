7b86aff525ab85a875aeb64aef0d112d9e96fbac
andy <andy@andydote.co.uk>
2016-10-15 20:38:35 +0300

    httpMethodOverride: remove external OwinMiddleware depenedency

--- Granger/Decorators/HttpMethodOverride.cs
+++ Granger/Decorators/HttpMethodOverride.cs
@@ -5,10 +5,11 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
 
 namespace Granger.Decorators
 {
-	public class HttpMethodOverride : OwinMiddleware
+	public class HttpMethodOverride
 	{
 		private static readonly string[] DefaultMethods = new[]
 		{
@@ -19,19 +20,23 @@ namespace Granger.Decorators
 			"head"
 		};
 
-		private HashSet<string> _allowedMethods;
+		private readonly AppFunc _next;
+		private readonly HashSet<string> _allowedMethods;
 
-		public HttpMethodOverride(OwinMiddleware next) : this(next, DefaultMethods)
+		public HttpMethodOverride(AppFunc next) : this(next, DefaultMethods)
 		{
 		}
 
-		public HttpMethodOverride(OwinMiddleware next, IEnumerable<string> allowedMethods) : base(next)
+		public HttpMethodOverride(AppFunc next, IEnumerable<string> allowedMethods)
 		{
+			_next = next;
 			_allowedMethods = new HashSet<string>(allowedMethods, StringComparer.OrdinalIgnoreCase);
 		}
 
-		public override async Task Invoke(IOwinContext context)
+		public async Task Invoke(IDictionary<string, object> environment)
 		{
+			var context = new OwinContext(environment);
+
 			if (context.Request.Method == HttpMethod.Post.Method)
 			{
 				var overrideMethod = context
@@ -52,7 +57,7 @@ namespace Granger.Decorators
 
 			}
 
-			await Next.Invoke(context);
+			await _next.Invoke(environment);
 		}
 	}
 }
