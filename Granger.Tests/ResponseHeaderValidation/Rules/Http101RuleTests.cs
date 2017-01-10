790f3c8a94d908595d39e8335400a0d9776fd627
andy <andy@andydote.co.uk>
2017-01-10 18:20:09 +0200

    responseHeaderValidation: implement http 101 rules

---
+++ Granger.Tests/ResponseHeaderValidation/Rules/Http101RuleTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Granger.ResponseHeaderValidation;
+using Granger.ResponseHeaderValidation.Rules;
+using Xunit;
+
+namespace Granger.Tests.ResponseHeaderValidation.Rules
+{
+	public class Http101RuleTests : RuleTestFor<Http101Rule>
+	{
+		protected override Http101Rule CreateRule() => new Http101Rule();
+		protected override void Before()
+		{
+			Response.StatusCode = (int)HttpStatusCode.SwitchingProtocols;
+			Response.Headers["Upgrade"] = "http/2.0";
+		}
+
+		[Theory]
+		[InlineData("Upgrade")]
+		public override void When_testing_headers(string header)
+		{
+			TestHeader(header);
+		}
+	}
+}
---
+++ Granger/ResponseHeaderValidation/Rules/Http101Rule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Owin;
+
+namespace Granger.ResponseHeaderValidation.Rules
+{
+	public class Http101Rule : IResponseRule
+	{
+		public IEnumerable<Violation> GetViolations(IOwinResponse response)
+		{
+			if (response.StatusCode == (int)HttpStatusCode.SwitchingProtocols && response.Headers.ContainsKey("Upgrade") == false)
+				yield return new Violation
+				{
+					Message = "The response was missing a recommend header: Upgrade",
+					Links = new Dictionary<string, HrefWrapper>
+					{
+						{ "rfc", new HrefWrapper { Href = new Uri("https://tools.ietf.org/html/rfc7231#section-6.2.2") } }
+					}
+				};
+		}
+	}
+}
