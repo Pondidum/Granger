059f0c4f401860bd238a1423e8f42d9ca583848f
andy <andy@andydote.co.uk>
2017-01-17 14:29:04 +0200

    responseHeader: basic http 206 implementation

---
+++ Granger.Tests/ResponseHeaderValidation/Rules/Http206RuleTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using Granger.ResponseHeaderValidation.Rules;
+using Xunit;
+
+namespace Granger.Tests.ResponseHeaderValidation.Rules
+{
+	public class Http206RuleTests : RuleTestForHeader<Http206Rule>
+	{
+		protected override Http206Rule CreateRule() => new Http206Rule();
+
+		protected override void Before()
+		{
+			Response.StatusCode = (int)HttpStatusCode.PartialContent;
+			Response.Headers["Content-Range"] = "bytes 0-14/28";
+		}
+
+		[Theory]
+		[InlineData("Content-Range")]
+		public override void When_testing_headers(string header)
+		{
+			TestHeader(header);
+		}
+	}
+}
---
+++ Granger/ResponseHeaderValidation/Rules/Http206Rule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+namespace Granger.ResponseHeaderValidation.Rules
+{
+	public class Http206Rule : HttpHeaderRule
+	{
+		public Http206Rule()
+			: base(HttpStatusCode.PartialContent, "Content-Range", new Uri("http://tools.ietf.org/html/rfc7233#section-4.1"))
+		{
+		}
+	}
+}
